dd6ad55952a5dbab52c904dbeb2a3f215cbfa423
Robert <robertpoparda@gmail.com>
2017-11-26 16:03:47 +0100

    DB for country-continent mappings added.

---
+++ demo/geo_country.pl
@@ -0,0 +1,76 @@
+belongsToContinent("Belgium", "Europe").
+belongsToContinent("Bulgaria", "Europe").
+belongsToContinent("Czech Republic", "Europe").
+belongsToContinent("Denmark", "Europe").
+belongsToContinent("Finland", "Europe").
+belongsToContinent("France", "Europe").
+belongsToContinent("Germany", "Europe").
+belongsToContinent("Greece", "Europe").
+belongsToContinent("Hungary", "Europe").
+belongsToContinent("Iceland", "Europe").
+belongsToContinent("Netherlands", "Europe").
+belongsToContinent("Norway", "Europe").
+belongsToContinent("Poland", "Europe").
+belongsToContinent("Romania", "Europe").
+belongsToContinent("Slovakia", "Europe").
+belongsToContinent("Slovenia", "Europe").
+belongsToContinent("Spain", "Europe").
+belongsToContinent("Switzerland", "Europe").
+belongsToContinent("Sweden", "Europe").
+belongsToContinent("West Germany", "Europe").
+belongsToContinent("Ireland", "Europe").
+belongsToContinent("Italy", "Europe").
+belongsToContinent("UK", "Europe").
+belongsToContinent("Turkey", "Europe").
+belongsToContinent("Afghanistan", "Asia").
+belongsToContinent("Cambodia", "Asia").
+belongsToContinent("China", "Asia").
+belongsToContinent("Hong Kong", "Asia").
+belongsToContinent("India", "Asia").
+belongsToContinent("Indonesia", "Asia").
+belongsToContinent("Iran", "Asia").
+belongsToContinent("Japan", "Asia").
+belongsToContinent("Russia", "Asia").
+belongsToContinent("Thailand", "Asia").
+belongsToContinent("New Zealand", "Asia").
+belongsToContinent("South Korea", "Asia").
+belongsToContinent("United Arab Emirates", "Asia").
+belongsToContinent("Taiwan", "Asia").
+belongsToContinent("Soviet Union", "Asia").
+belongsToContinent("Israel", "Asia").
+belongsToContinent("Pakistan", "Asia").
+belongsToContinent("Kyrgyzstan", "Asia").
+belongsToContinent("Philippines", "Asia").
+belongsToContinent("Argentina", "America-Pd").
+belongsToContinent("Argentina", "America").
+belongsToContinent("Aruba", "America-Pd").
+belongsToContinent("Aruba", "America").
+belongsToContinent("Brazil", "America-Pd").
+belongsToContinent("Brazil", "America").
+belongsToContinent("Chile", "America-Pd").
+belongsToContinent("Chile", "America").
+belongsToContinent("Colombia", "America-Pd").
+belongsToContinent("Colombia", "America").
+belongsToContinent("Peru", "America-Pd").
+belongsToContinent("Peru", "America").
+belongsToContinent("Panama", "America-Pd").
+belongsToContinent("Panama", "America").
+belongsToContinent("Bahamas", "America").
+belongsToContinent("Bahamas", "America-Pn").
+belongsToContinent("Canada", "America").
+belongsToContinent("Canada", "America-Pn").
+belongsToContinent("USA", "America").
+belongsToContinent("USA", "America-Pn").
+belongsToContinent("Mexico", "America").
+belongsToContinent("Mexico", "America-Pn").
+belongsToContinent("Dominican Republic", "America").
+belongsToContinent("Dominican Republic", "America-Pn").
+belongsToContinent("Georgia", "America").
+belongsToContinent("Georgia", "America-Pn").
+belongsToContinent("Cameroon", "Africa").
+belongsToContinent("Egypt", "Africa").
+belongsToContinent("Kenya", "Africa").
+belongsToContinent("Nigeria", "Africa").
+belongsToContinent("South Africa", "Africa").
+belongsToContinent("Libya", "Africa").
+belongsToContinent("Australia", "Australia").
